b2348e404df64f0dac1cbedc92442e9a8ea7d7f5
Sebastian Choren <sebastianchoren@gmail.com>
2014-10-19 18:02:05 -0300

    fixed tests with new API errors

--- tests/White_ExceptionsTest.php
+++ tests/White_ExceptionsTest.php
@@ -64,10 +64,10 @@ class White_ExceptionsTest extends \PHPUnit_Framework_TestCase
     White::setApiKey('sk_test_1234567890abcdefghijklmnopq');
 
     $data = array(
-      "amount" => 10.500,
+      "amount" => -1.30,
       "currency" => "bhd",
       "card" => array(
-        "number" => "1",
+        "number" => "4242424242424242",
         "exp_month" => 12,
         "exp_year" => 2014,
         "cvv" => "123"
