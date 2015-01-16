1e85d8b6a1535d5e6913d804ece03e4f301c182c
Yazin S <i@yaz.in>
2015-01-16 20:32:57 +0400

    Removed Refunds reference

--- White.php
+++ White.php
@@ -3,7 +3,7 @@
 //File to use when using lib without composer.
 require_once 'src/White.php';
 require_once 'src/White/Charge.php';
-require_once 'src/White/Refund.php';
+// require_once 'src/White/Refund.php';
 require_once 'src/White/Error.php';
 require_once 'src/White/Error/Authentication.php';
 require_once 'src/White/Error/Banking.php';
