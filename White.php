cccd87b91f560f27d9b5e5506b0cafe3c4ed449c
Yazin S <i@yaz.in>
2015-01-26 11:55:21 +0300

    Added Customers reference (thanks Alexey)

--- White.php
+++ White.php
@@ -4,6 +4,7 @@
 require_once 'src/White.php';
 require_once 'src/White/Charge.php';
 require_once 'src/White/Error.php';
+require_once 'src/White/Customer.php';
 require_once 'src/White/Error/Authentication.php';
 require_once 'src/White/Error/Banking.php';
 require_once 'src/White/Error/Processing.php';
